e477c600e4b1fc453d60078e07cf20ada76d6659
Moustafa <mustafa.motawei@gmail.com>
2024-07-17 15:39:39 +0400

    rename obj owner permission

--- imgboard_app/api/permissions.py
+++ imgboard_app/api/permissions.py
@@ -6,7 +6,7 @@ class AdminOrReadOnly(permissions.IsAdminUser):
             return True
         return bool(request.user and request.user.is_staff)
 
-class PostUserOrReadOnly(permissions.BasePermission):
+class ObjUserOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
