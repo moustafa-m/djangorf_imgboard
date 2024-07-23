632839ea7fb0b0e38daf8db1e80a342a64a69c1f
Moustafa <mustafa.motawei@gmail.com>
2024-07-23 15:15:48 +0400

    use api/ at URL start

--- imgboard/urls.py
+++ imgboard/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users_app.api.urls')),
-    path('board/', include('imgboard_app.api.urls')),
+    path('api/user/', include('users_app.api.urls')),
+    path('api/board/', include('imgboard_app.api.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
